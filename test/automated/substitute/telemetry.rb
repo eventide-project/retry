1c951468f4bb7a53f3ae2c5f5bbc88eebc27ebc7
Scott Bellware <sbellware@gmail.com>
2022-12-06 10:31:43 -0600

    Vestigial code is removed from telemetry configuration test

--- test/automated/substitute/telemetry.rb
+++ test/automated/substitute/telemetry.rb
@@ -5,9 +5,6 @@ context "Substitute" do
     context "Sink" do
       rtry = Retry::Substitute.build
 
-      millisecond_intervals = [1, 11]
-      errors = [Retry::Controls::ErrorA, Retry::Controls::ErrorB]
-
       test "Is configured" do
         assert(rtry.sink.instance_of?(Retry::Telemetry::Sink))
       end
